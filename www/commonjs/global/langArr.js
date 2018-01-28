cea9178fc6a51f660b10ac742eb9f090a5434489
vcshing <vcshing2005@hotmail.com>
2018-01-28 16:52:41 +0800

    asasd

--- www/commonjs/global/langArr.js
+++ www/commonjs/global/langArr.js
@@ -6,12 +6,12 @@ var admobid = {};
 
 
 appConfigArr={
-  "appName" :	"投票",
-  "appDescription"	:	"投票",
-  "androidAppLink"	:"https://play.google.com/store/apps/details?id=com.skyexplorer.vote",
-  "androidAdmobBannerID"	:"ca-app-pub-3715336230214756/7051579428",
-  "androidAdmobInterstitialID"	:"ca-app-pub-3715336230214756/9103027690",
-  "androidAdmobBonusID"	:"ca-app-pub-3715336230214756/9768763179",
+  "appName" :	"No Contacts List with WhatsApp",
+  "appDescription"	:	"No Contacts List with WhatsApp",
+  "androidAppLink"	:"https://play.google.com/store/apps/details?id=com.skyexplorer.whatsAdd",
+  "androidAdmobBannerID"	:"ca-app-pub-3715336230214756/4600266782",
+  "androidAdmobInterstitialID"	:"ca-app-pub-3715336230214756/4642652907",
+  "androidAdmobBonusID"	:"ca-app-pub-3715336230214756/1016244997",
   "isTesting" : false
 }
 appArr={
